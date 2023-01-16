f1480418c592dfd9c5c53b2737455281f7ef7e66
Pavel Zolotin <zolotinpavel@gmail.com>
2023-01-16 21:11:41 +0500

    add fetch data with axios

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,55 @@
+import {useEffect, useState} from 'react'
+
 import Logo from '../logo/Logo'
 import Search from '../search/Search'
 import ToggleMode from '../toggleMode/ToggleMode'
 import './header.scss'
 
-const Header = ({logo, searchTerm, setSearchTerm, clearSearchInput, toggleTheme, toggleIsClicked}) => {
+const API_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
+
+const Header = ({getMovies, API_URL}) => {
+    const [searchTerm, setSearchTerm] = useState('')
+    const searchResult = `${API_SEARCH}${searchTerm}`
+    const [theme, setTheme] = useState(
+        localStorage.getItem('theme') || 'dark'
+    )
+
+    const [logo, setLogoColor] = useState(
+        localStorage.getItem('logo') || 'lighten'
+    )
+
+    const themeMode = theme === 'dark' ? 'dark-mode' : 'light-mode'
+    const toggleIsClicked = theme === 'light' ? true : ''
+
+    const toggleTheme = () => {
+        if (theme === 'dark') {
+            setTheme('light')
+            setLogoColor('darken')
+        } else {
+            setTheme('dark')
+            setLogoColor('lighten')
+        }
+    }
+
+    const clearSearchInput = () => {
+        setSearchTerm('')
+        getMovies(API_URL)
+    }
+
+    useEffect(() => {
+        localStorage.setItem('theme', theme)
+        localStorage.setItem('logo', logo)
+        document.body.className = theme
+    }, [theme, logo])
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            getMovies(searchResult)
+        } else {
+            getMovies(API_URL)
+        }
+    }, [searchTerm, searchResult])
+
     return (
         <div className="app__header">
             <Logo
