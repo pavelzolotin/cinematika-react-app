9b104e91b69257b6a7bd22036489d5980a73445c
Pavel Zolotin <zolotinpavel@gmail.com>
2023-01-23 20:15:46 +0500

    add new styles to skeleton component

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -4,14 +4,14 @@ function Skeleton() {
     return (
         <ContentLoader
             speed={2}
-            width={285}
+            width={300}
             height={500}
             viewBox="0 0 285 500"
             backgroundColor="#343739"
-            foregroundColor="#232526ab"
+            foregroundColor="#565656d1"
         >
-            <rect x="15" y="25" rx="10" ry="10" width="270" height="320"/>
-            <rect x="15" y="375" rx="10" ry="10" width="270" height="60"/>
+            <rect x="5" y="25" rx="10" ry="10" width="275" height="320"/>
+            <rect x="5" y="375" rx="10" ry="10" width="275" height="60"/>
         </ContentLoader>
     )
 }
