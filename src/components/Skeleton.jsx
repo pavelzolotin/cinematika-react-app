1eaf5677379be95fe62d24e7b71d8eb29cd7c31b
Pavel Zolotin <zolotinpavel@gmail.com>
2023-03-12 17:44:58 +0500

    change Skeleton styles

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -7,11 +7,11 @@ const Skeleton = () => {
             width={300}
             height={500}
             viewBox="0 0 300 500"
-            backgroundColor="#817c7c"
-            foregroundColor="#444444d4"
+            backgroundColor="#7c7c7c"
+            foregroundColor="#a5a5a5"
         >
-            <rect x="0" y="0" rx="10" ry="0" width="300" height="340"/>
-            <rect x="0" y="390" rx="0" ry="10" width="300" height="60"/>
+            <rect x="0" y="0" rx="5" ry="0" width="300" height="340"/>
+            <rect x="0" y="390" rx="0" ry="5" width="300" height="60"/>
         </ContentLoader>
     );
 };
